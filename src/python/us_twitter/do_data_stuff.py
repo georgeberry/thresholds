0be70789dae962cf660e08ad1e15cb9f60590ce7
George Berry <george.e.berry@gmail.com>
2017-03-03 15:29:33 -0500

    Sub

--- src/python/us_twitter/do_data_stuff.py
+++ src/python/us_twitter/do_data_stuff.py
@@ -162,7 +162,7 @@ if __name__ == '__main__':
                     for tweet in data['tweets']:
                         tweet_tags = set()
                         tid = tweet['id_str']
-                        text = tweet['text'].replace('\t', ' ')
+                        text = re.sub(r'\s+', ' ', tweet['text'])
                         created_at = create_timestamp(tweet['created_at'])
                         # Skip tweets without a creation time
                         if len(created_at) < 10:
