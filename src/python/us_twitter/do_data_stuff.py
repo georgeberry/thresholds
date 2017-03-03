95f561520a6ea8b7861dd6068ef130f28c9456f1
George Berry <george.e.berry@gmail.com>
2017-03-03 16:54:37 -0500

    Revert

--- src/python/us_twitter/do_data_stuff.py
+++ src/python/us_twitter/do_data_stuff.py
@@ -3,9 +3,6 @@ import psycopg2
 import ujson as json
 import glob
 import bz2
-import re
-import csv
-import io
 from find_users_for_tags import TW_DATE_FMT, PS_DATE_FMT, create_timestamp
 
 """
@@ -14,20 +11,17 @@ Plan:
     2. Insert these user timelines into a Tweets dict
     3. Go through all edges and add those edges to RawEdges
     4.
-
 See schema.sql for the postgres schema
 We have four tables
     Hashtags: hashtag usage counts
     Tweets: all tweets, with timestamps, mentions, and hashtags extracted
     RawEdges: all edges compiled by Patrick
     TimestampEdges:
-
 Analysis plan:
     For each hashtag we need to look at its first use within a user, then look
     at the tweet that immediately preceeded that. This gives us a time interval
     to work with. In that interval, if exactly one neighbor activates, then
     the individual is correctly measured.
-
     There is probably a more efficient algorithm to do this.
     Would be great to, in one shot, get threshold and whether its measure or not.
 """
@@ -41,7 +35,6 @@ with open('config.json', 'r') as f:
     # TIMELINE_FOLDER =
     EDGELIST_FILE = j['edgelist']
     SUCCESS_USER_PATTERN = j['success_pattern']
-    OUTFILE_NAME = j['output_file']
 
 # Postgres functions
 
@@ -71,11 +64,9 @@ def psql_insert_many(db, table, data):
     data: a list of tuples containing data for the table
         CAUTION! data tuples must be in the correct order for the table
         ADVICE! You should correctly type your data before it reaches this point
-
     We mogrify in advance to get better speed
     This will store your ENTIRE query in memory in python. This is NOT efficient,
         so consider batching a higher level if you have a really giant query
-
     Check here: http://stackoverflow.com/questions/8134602/
                 psycopg2-insert-multiple-rows-with-one-query
     """
@@ -107,21 +98,16 @@ if __name__ == '__main__':
     """
     # List of tuples, (htag, count)
     ht_data = []
-
     with open(HTAG_COUNT_FILE, 'r') as f:
         for line in f:
             htag, count = line.strip('\n').split('\t')
             ht_data.append((str(htag), int(count)))
-
     print('Here\'s what the data looks like')
     print(ht_data[:10])
-
     psql_insert_many(db, 'Hashtags', ht_data)
     print('Inserted hashtags successfully!')
-
     del ht_data
-
-
+    """
 
     # Insert edges #
 
@@ -144,37 +130,49 @@ if __name__ == '__main__':
         psql_insert_many(db, 'Edges', edge_data)
 
     del edge_data
-    """
 
     # Insert tweets #
     print('Starting insert.')
 
-    tweet_count = 0
+    tweet_data = []
+    count = 0
 
     file_list = glob.glob(SUCCESS_USER_PATTERN)
-    with io.open(OUTFILE_NAME, 'wb') as outfile:
-        for fname in file_list:
-            with bz2.open(fname, 'r') as f:
-                for line in f:
-                    uid, data_json = line.split(b'\t', 1)
-                    uid = uid.strip(b'"')
-                    data = json.loads(data_json)
-                    for tweet in data['tweets']:
-                        tweet_tags = set()
-                        tid = bytes(tweet['id_str'], 'utf8')
-                        text = re.sub(b'\s+', b' ', bytes(tweet['text'], 'utf8'))
-                        text = re.sub(b'\\', b"\\\\", text)
-                        created_at = create_timestamp(tweet['created_at'])
-                        # Skip tweets without a creation time
-                        if len(created_at) < 10:
-                            continue
-                        for tag in tweet['entities']['hashtags']:
-                            tweet_tags.add(tag['text'])
-                        if len(tweet_tags) == 0:
-                            tup = (uid, tid, text, created_at, b"")
-                            outfile.write(b'\t'.join(tup) + b'\n')
-                        else:
-                            for tag in tweet_tags:
-                                tup = (uid, tid, text, created_at, tag)
-                                outfile.write(b'\t'.join(tup) + b'\n')
-            print('Done with file {}!'.format(fname))
+    for fname in file_list:
+        with bz2.open(fname, 'r') as f:
+            for line in f:
+                uid, data_json = line.split(b'\t', 1)
+                uid = int(uid.strip(b'"'))
+                data = json.loads(data_json)
+                for tweet in data['tweets']:
+                    tweet_tags = set()
+                    tid = tweet['id_str']
+                    text = tweet['text']
+                    created_at = create_timestamp(tweet['created_at'])
+                    for tag in tweet['entities']['hashtags']:
+                        tweet_tags.add(tag['text'])
+                    if len(tweet_tags) == 0:
+                        tweet_data.append((
+                            uid,
+                            tid,
+                            text,
+                            created_at,
+                            None,
+                        ))
+                        count += 1
+                    else:
+                        for tag in tweet_tags:
+                            tweet_data.append((
+                                uid,
+                                tid,
+                                text,
+                                created_at,
+                                tag,
+                            ))
+                            count += 1
+                    if count > 1000000:
+                        psql_insert_many(db, 'SuccessTweets', tweet_data)
+                        tweet_data = []
+                        count = 0
+                        print('Inserted another 1m!')
+            print('Finished file {}!'.format(fname))
