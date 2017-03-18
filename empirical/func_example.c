61d041d68570fe3296e1fdc77f04cb787a7f707d
George Berry <george.e.berry@gmail.com>
2017-03-17 21:42:50 -0400

    update to c test code

--- empirical/func_example.c
+++ empirical/func_example.c
@@ -7,14 +7,20 @@
 PG_MODULE_MAGIC;
 #endif
 
-/* by value */
+/*
+CREATE OR REPLACE FUNCTION
+  add_one( INT )
+RETURNS INT
+AS 'func_example.so', 'add_one'
+LANGUAGE C STRICT IMMUTABLE;
+ */
 
 PG_FUNCTION_INFO_V1(add_one);
 
 Datum
 add_one(PG_FUNCTION_ARGS)
 {
-    int32   arg = PG_GETARG_INT32(0);
+    int64   arg = PG_GETARG_INT64(0);
 
-    PG_RETURN_INT32(arg + 1);
+    PG_RETURN_INT64(arg + 1);
 }
