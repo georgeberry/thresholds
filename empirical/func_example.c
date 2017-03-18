b888dbe36e3ea6febe2c393d040a75cfafeca69f
George Berry <george.e.berry@gmail.com>
2017-03-17 20:18:13 -0400

    example c function

---
+++ empirical/func_example.c
@@ -0,0 +1,20 @@
+#include "postgres.h"
+#include <string.h>
+#include "fmgr.h"
+#include "utils/geo_decls.h"
+
+#ifdef PG_MODULE_MAGIC
+PG_MODULE_MAGIC;
+#endif
+
+/* by value */
+
+PG_FUNCTION_INFO_V1(add_one);
+
+Datum
+add_one(PG_FUNCTION_ARGS)
+{
+    int32   arg = PG_GETARG_INT32(0);
+
+    PG_RETURN_INT32(arg + 1);
+}
