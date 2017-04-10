91e1bb77d76efd14d84cb73da867b78d8e471809
George Berry <george.e.berry@gmail.com>
2017-04-10 14:23:58 -0400

    more small graph code

--- simulation/python/small_graph_inventory.py
+++ simulation/python/small_graph_inventory.py
@@ -12,3 +12,52 @@ from networkx.generators.atlas import *
 
 
 a = graph_atlas_g()
+
+
+
+import networkx as nx
+
+import matplotlib.pyplot as plt
+
+G=nx.cycle_graph(4)
+
+H=nx.path_graph(4)
+
+plt.figure(1)
+
+nx.draw(G)
+
+plt.figure(2)
+
+nx.draw(H)
+
+plt.show()
+
+
+
+
+G1 = nx.balanced_tree(2,1)
+G2 = nx.balanced_tree(2,2)
+
+
+elarge1 =[(u,v) for (u,v,d) in G1.edges(data=True)]
+elarge2 =[(u,v) for (u,v,d) in G2.edges(data=True)]
+
+
+pos1=nx.spring_layout(G1)
+pos2=nx.spring_layout(G2)
+
+
+for k,v in pos2.items():
+    # Shift the x values of every node by 10 to the right
+    v[0] = v[0] + 10
+
+nx.draw_networkx_nodes(G1,pos1,node_size=50,node_color='b')
+nx.draw_networkx_edges(G1,pos1,edgelist=elarge1,width=1,style='solid')
+
+
+nx.draw_networkx_nodes(G2,pos2,node_size=10)
+nx.draw_networkx_edges(G2,pos2,edgelist=elarge2,width=1)
+
+plt.show() # display
+plt.draw()
