0e8527b2b77ef62e406c4740deeaf35c6a80fdb0
George Berry <george.e.berry@gmail.com>
2017-04-17 14:29:45 -0400

    Specific tags

--- empirical/bash/relevant_timelines_from_raw.sh
+++ empirical/bash/relevant_timelines_from_raw.sh
@@ -20,75 +20,27 @@ cat test1.json | jq '.tweets[] | select(.entities.hashtags[].text | contains("Lo
 
 # Timelines where at least one tweet contains a hashtag in the list
 # Best candidate
-cat test1.json | jq '.
+
+{"obama2012": 1, "election2012": 1, "kony2012": 1, "romney": 1, "rippaulwalker": 1, "teamobama": 1, "bringbackourgirls": 1, "trayvonmartin": 1, "hurricanesandy": 1, "cantbreathe": 1, "miley": 1, "olympics2014": 1, "prayfornewtown": 1, "goodbyebreakingbad": 1, "governmentshutdown": 1, "riprobinwilliams": 1, "romneyryan2012": 1, "harlemshake": 1, "euro2012": 1, "marriageequality": 1, "benghazi": 1, "debate2012": 1, "newtown": 1, "linsanity": 1, "zimmerman": 1, "betawards2014": 1, "justicefortrayvon": 1, "samelove": 1, "worldcupfinal": 1, "prayersforboston": 1, "nobama": 1, "ferguson": 1, "springbreak2014": 1, "drawsomething": 1, "nfldraft2014": 1, "romney2012": 1, "snowden": 1, "replaceashowtitlewithtwerk": 1, "inaug2013": 1, "ivoted": 1, "trayvon": 1, "ios6": 1, "voteobama": 1, "jodiarias": 1, "windows8": 1, "mentionsomebodyyourethankfulfor": 1, "sharknado2": 1, "gop2012": 1, "whatdoesthefoxsay": 1, "firstvine": 1}
+
+cat test1.json | jq -r '{"obama2012": 1, "election2012": 1, "kony2012": 1, "romney": 1, "rippaulwalker": 1, "teamobama": 1, "bringbackourgirls": 1, "trayvonmartin": 1, "hurricanesandy": 1, "cantbreathe": 1, "miley": 1, "olympics2014": 1, "prayfornewtown": 1, "goodbyebreakingbad": 1, "governmentshutdown": 1, "riprobinwilliams": 1, "romneyryan2012": 1, "harlemshake": 1, "euro2012": 1, "marriageequality": 1, "benghazi": 1, "debate2012": 1, "newtown": 1, "linsanity": 1, "zimmerman": 1, "betawards2014": 1, "justicefortrayvon": 1, "samelove": 1, "worldcupfinal": 1, "prayersforboston": 1, "nobama": 1, "ferguson": 1, "springbreak2014": 1, "drawsomething": 1, "nfldraft2014": 1, "romney2012": 1, "snowden": 1, "replaceashowtitlewithtwerk": 1, "inaug2013": 1, "ivoted": 1, "trayvon": 1, "ios6": 1, "voteobama": 1, "jodiarias": 1, "windows8": 1, "mentionsomebodyyourethankfulfor": 1, "sharknado2": 1, "gop2012": 1, "whatdoesthefoxsay": 1, "firstvine": 1} as $x
+| .
 | .user[0].id_str as $uid
 | select(
-  [.tweets[].entities.hashtags[].text | in({"nofilter": 1})]
+  [.tweets[].entities.hashtags[].text | ascii_downcase | in($x)]
   | any)
 | .tweets[]
-| "\(.text)\t\($uid)\t\(.id_str)\t\(.created_at)"' > test1.tsv
-
-
-
-
-
-
-
-## more testing ##
-
-cat test1.json | jq '.
-| .user[0].id_str as $uid
-| .tweets[].entities.hashtags[].text
-| select(map(in({"nofilter": 1})) | any)
-| .tweets[]
-| "\(.text)\t\($uid)\t\(.id_str)\t\(.created_at)"' > test1.tsv
-
-# slower
-cat test1.json | jq '.
-| .user[0].id_str as $uid
-| .tweets[]
-| {uid: $uid, text: .text, created_at: .created_at, hashtag: .entities.hashtags[].text}
-| "\(.text)\t\($uid)\t\(.id_str)\t\(.created_at)"' > test1.tsv
-
-cat test1.json | jq '.
-| .user[0].id_str as $uid
-| select(
-  [.tweets[].entities.hashtags[].text | in({"nofilter": 1})]
-  | any)
-| .tweets[]
-| "\(.text)\t\($uid)\t\(.id_str)\t\(.created_at)"' > test1.tsv
-
+| "\($uid)\t\(.created_at)\t\(.id_str)\t\(.text)\t\([.entities.hashtags[].text | ascii_downcase])"' > test1.tsv
 
 
 
 
 # 57s
-time bzcat part-00000.bz2 | head -n1000 | cut -f2 | jq '.
+time bzcat part-00000.bz2 | cut -f2 | jq -r '{"obama2012": 1, "election2012": 1, "kony2012": 1, "romney": 1, "rippaulwalker": 1, "teamobama": 1, "bringbackourgirls": 1, "trayvonmartin": 1, "hurricanesandy": 1, "cantbreathe": 1, "miley": 1, "olympics2014": 1, "prayfornewtown": 1, "goodbyebreakingbad": 1, "governmentshutdown": 1, "riprobinwilliams": 1, "romneyryan2012": 1, "harlemshake": 1, "euro2012": 1, "marriageequality": 1, "benghazi": 1, "debate2012": 1, "newtown": 1, "linsanity": 1, "zimmerman": 1, "betawards2014": 1, "justicefortrayvon": 1, "samelove": 1, "worldcupfinal": 1, "prayersforboston": 1, "nobama": 1, "ferguson": 1, "springbreak2014": 1, "drawsomething": 1, "nfldraft2014": 1, "romney2012": 1, "snowden": 1, "replaceashowtitlewithtwerk": 1, "inaug2013": 1, "ivoted": 1, "trayvon": 1, "ios6": 1, "voteobama": 1, "jodiarias": 1, "windows8": 1, "mentionsomebodyyourethankfulfor": 1, "sharknado2": 1, "gop2012": 1, "whatdoesthefoxsay": 1, "firstvine": 1} as $x
+| .
 | .user[0].id_str as $uid
 | select(
-  [.tweets[].entities.hashtags[].text | in({"nofilter": 1})]
+  [.tweets[].entities.hashtags[].text | ascii_downcase | in($x)]
   | any)
 | .tweets[]
-| "\(.text)\t\($uid)\t\(.id_str)\t\(.created_at)"' > ~/test1.tsv
-
-time bzcat part-00000.bz2 | head -n1000 | cut -f2 | jq '.
-| .user[0].id_str as $uid
-| select(
-  [.tweets[].entities.hashtags[].text | in({"nofilter": 1})]
-  | any)' > ~/test1.tsv
-
-
-# 1m1s
-time bzcat part-00000.bz2 | head -n1000 | cut -f2 | jq '.
-| .user[0].id_str as $uid
-| .tweets[]
-| {uid: $uid, text: .text, created_at: .created_at, hashtag: .entities.hashtags[].text}
-| "\(.text)\t\($uid)\t\(.id_str)\t\(.created_at)"' > ~/test1.tsv
-
-
-# 1m1s
-time bzcat part-00000.bz2 | head -n1000 | cut -f2 | jq '.
-| .user[0].id_str as $uid
-| .tweets[]
-| {uid: $uid, text: .text, created_at: .created_at, hashtag: .entities.hashtags[].text}
-| "\(.text)\t\($uid)\t\(.id_str)\t\(.created_at)"' > ~/test1.tsv
+| "\($uid)\t\(.created_at)\t\(.id_str)\t\(.text)\t\([.entities.hashtags[].text | ascii_downcase])"' > ~/test1.tsv
