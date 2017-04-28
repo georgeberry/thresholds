11973359e4ef6abc14d6c806b43c9f72377d72e1
George Berry <george.e.berry@gmail.com>
2017-04-28 14:38:13 -0400

    Should be operational

--- empirical/insert_tweets.py
+++ empirical/insert_tweets.py
@@ -15,9 +15,17 @@ Tweets table looks like this:
     hashtag varchar(140)
 
 To test:
-    python insert_tweets.py /Volumes/Vostok/class/twitter_data/thresholds/part-00000.tsv.bz2
+python insert_tweets.py /Volumes/Vostok/class/twitter_data/thresholds/part-00000.tsv.bz2
+
+
+# find /Volumes/Starbuck/class/twitter_data/modified_essential/US_GB_CA_AU_NZ_SG/part-014*.bz2 -print0 | xargs -0 -n1 -P6 -- bash -c './timelines_from_raw.sh "$0"'
+
 """
 
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s',
+    level=logging.INFO,
+)
 
 class BatchYielder(object):
     """
@@ -36,78 +44,73 @@ class BatchYielder(object):
         self.cache = ''
         self.current_batch = []
 
-    def process_full_line(self, uid, tstamp_str, tid, text, htag_str):
+    def process_full_line(self, line):
         """
         If we know we have all 5 elements, process them and potentially yield
         """
-        timestsamp = create_timestamp(tstamp_str)
-        if len(htag_str) > 2:
-            hashtags = json.loads(htag_str)
-        else:
+        uid, tstamp_str, tid, text_plus_htag_str = line.split('\t', 3)
+        text, htag_str = text_plus_htag_str.rsplit('\t', 1)
+        timestamp = create_timestamp(tstamp_str)
+        hashtags = json.loads(htag_str)
+        if len(hashtags) == 0:
             hashtags = [None]
         self.count += 1
+        if self.count % 100000 == 0:
+            logging.info("Processed {} from {}".format(self.count, self.fname))
         for hashtag in hashtags:
             self.current_batch.append((
                 uid,
                 tid,
-                timestsamp,
+                timestamp,
                 hashtag,
             ))
 
     def __iter__(self):
-        with bz2.open(self.fname, 'r') as infile:
+        """
+        This looks complicated, but it's not
+        We need to deal with cases where people put whitespase chars in tweets
+        To do this we need to know when a line is really done
+        The easiest way to do this is to see if the second element of a line is
+        the date. If create_timestamp(line.split()[1]) works, then the prev
+        tweet is done.
+        """
+        with bz2.open(self.fname, 'rt', encoding='utf8') as infile:
+            # lines can have a valid beginning or not, assessed with timestamp
             for line in infile:
-                self.count += 1
-                uid, tstamp_str, tid, text, htag_str = line.split(b'\t')
-                if self.count % 10000 == 0:
-                    print(self.count)
+                valid_beginning = None
+                # assess whether we have a line beginning or not
+                try:
+                    timestamp = create_timestamp(line.split('\t')[1])
+                    valid_beginning = True
+                except (ValueError, IndexError, TypeError):
+                    valid_beginning = False
 
+                # save previous line
+                if valid_beginning and len(self.cache) > 0:
+                    self.process_full_line(self.cache)
+                    self.cache = ''
 
-            """
-            for line in infile:
-                # if cache > 0, try to tab split
-                # else add to cache
-                if len(self.cache) > 0:
+                # we have a line beginning
+                # if we have a complete line, save it
+                # if we have a partial line
+                if valid_beginning:
                     try:
-                        uid, tstamp_str, tid, text, htag_str = \
-                            self.cache.split('\t')
-                        self.process_full_line(
-                            uid,
-                            tstamp_str,
-                            tid,
-                            text,
-                            htag_str,
-                        )
-                        self.cache = ''
-                    except ValueError:
-                        self.cache += line
-                # if len(cache) == 0
-                # try to split
-                else:
-                    try:
-                        uid, tstamp_str, tid, text, htag_str = line.split('\t')
-                        self.process_full_line(
-                            uid,
-                            tstamp_str,
-                            tid,
-                            text,
-                            htag_str,
-                        )
-                    except ValueError:
-                        self.cache += line
-                # yield here
-                print(self.count)
-                if len(self.current_batch) >= self.batch_size:
-                    print('Yielded {}!'.format(self.count))
+                        self.process_full_line(line)
+                    except (ValueError, IndexError, TypeError):
+                        self.cache = line
+
+                # line intermezzo, add to cache
+                if not valid_beginning and len(self.cache) > 0:
+                    self.cache = ''.join([self.cache, line])
+
+                if not valid_beginning and len(self.cache) == 0:
+                    raise ValueError
+
+                if len(self.current_batch) > self.batch_size:
                     yield self.current_batch
                     self.current_batch = []
             else:
                 yield self.current_batch
-            """
-
-by = BatchYielder(fname)
-for batch in by:
-    pass
 
 if __name__ == '__main__':
     db = psql_connect()
@@ -115,5 +118,4 @@ if __name__ == '__main__':
     fname = sys.argv[1]
     by = BatchYielder(fname)
     for batch in by:
-        pass
-        # psql_insert_many(db, table, batch)
+        psql_insert_many(db, table, batch)
